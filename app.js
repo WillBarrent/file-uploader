70c2e7f6662fd0403fae2f83131a4d9e22e823d4
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-06-05 22:46:31 +0500

    Testing passport

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 
+const path = require("node:path");
 const express = require("express");
 const app = express();
+const passport = require('passport');
 
 const expressSession = require("express-session");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const { PrismaClient } = require("./generated/prisma");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   expressSession({
@@ -22,6 +29,48 @@ app.use(
     }),
   })
 );
+
+require("./passport");
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', async (req, res) => {
+    const prisma = new PrismaClient();
+
+    const allUsers = await prisma.user.findMany();
+
+    await prisma.$disconnect();
+    
+    console.log(allUsers);
+
+    res.render("sign-up");
+});
+
+app.post("/sign-up", async (req, res) => {
+
+  const {username, password} = req.body;
+  const prisma = new PrismaClient();
+
+  await prisma.user.create({
+    data: {
+      username: username,
+      password: password,
+    }
+  });
+
+  res.redirect("/");
+});
+
+app.get("/login", async (req, res) => {
+  res.render("login");
+})
+
+app.post("/login", passport.authenticate("local", {
+  failureRedirect: "/login",
+  successRedirect: "/",
+  failureMessage: true,
+}));
 
 const PORT = 3000;
 
