08f4b92d1b2310b741e82ea91b639784586e2638
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-07-05 15:53:18 +0500

    Add log out route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,6 +6,7 @@ const {
   signUpPost,
   loginGet,
   loginPost,
+  logOutGet,
 } = require("../controllers/authController");
 
 authRouter.get("/sign-up", signUpGet);
@@ -13,5 +14,7 @@ authRouter.post("/sign-up", signUpPost);
 
 authRouter.get("/login", loginGet);
 authRouter.post("/login", loginPost);
+
+authRouter.get("/log-out", logOutGet)
 
 module.exports = authRouter;
