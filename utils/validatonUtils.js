8072151b48d73f4ec7b75c317983f1402de00ca5
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-07-20 17:44:10 +0500

    Fill arrays with validation methods

--- utils/validatonUtils.js
+++ utils/validatonUtils.js
@@ -1,12 +1,54 @@
 const { body } = require("express-validator");
 
-const signUpValidation = [];
+const signUpValidation = [
+  body("username")
+    .notEmpty()
+    .withMessage("Username must be filled")
+    .isAlpha()
+    .withMessage("Username must only contain alphabetic letters.")
+    .isLength({ min: 3, max: 15 })
+    .withMessage("The length of the username is between 2 and 15 included."),
+  body("password")
+    .notEmpty()
+    .withMessage("Password must be filled")
+    .isAlphanumeric()
+    .withMessage("Password must contain alphabetic letters and numbers.")
+    .isLength({ min: 8 })
+    .withMessage("Password must contain at least 8 characters."),
+];
 
-const loginValidation = [];
+const loginValidation = [
+  body("username")
+    .notEmpty()
+    .withMessage("Username must be filled")
+    .isAlpha()
+    .withMessage("Username must only contain alphabetic letters.")
+    .isLength({ min: 3, max: 15 })
+    .withMessage("The length of the username is between 2 and 15 included."),
+  body("password")
+    .notEmpty()
+    .withMessage("Password must be filled")
+    .isAlphanumeric()
+    .withMessage("Password must contain alphabetic letters and numbers.")
+    .isLength({ min: 8 })
+    .withMessage("Password must contain at least 8 characters."),
+];
 
-const newFolderValidation = [];
+const newFolderValidation = [
+  body("newFolderName")
+    .notEmpty()
+    .withMessage("Folder name field must be filled")
+    .isLength({ max: 35 })
+    .withMessage("The length cannot be more than 35 letters"),
+];
 
-const editFolderValidation = [];
+const editFolderValidation = [
+  body("newFolderName")
+    .notEmpty()
+    .withMessage("Folder name field must be filled")
+    .isLength({ max: 35 })
+    .withMessage("The length cannot be more than 35 letters"),
+];
 
 module.exports = {
   signUpValidation,
