c9c53038da0bda3d1fcf7246c149f0c2c66f379d
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2025-07-20 17:31:21 +0500

    Add validation utils for input fields

---
+++ utils/validatonUtils.js
@@ -0,0 +1,16 @@
+const { body } = require("express-validator");
+
+const signUpValidation = [];
+
+const loginValidation = [];
+
+const newFolderValidation = [];
+
+const editFolderValidation = [];
+
+module.exports = {
+  signUpValidation,
+  loginValidation,
+  newFolderValidation,
+  editFolderValidation,
+};
